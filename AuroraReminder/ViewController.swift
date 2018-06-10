cfab919f20c9feaba24a1e831acfc2dd2a258ac4
jennyqq <jennifer@drawbridgenetworks.com>
2018-06-10 10:06:57 -0400

    Initial Commit

---
+++ AuroraReminder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AuroraReminder
+//
+//  Created by Qiu Zhang on 6/10/18.
+//  Copyright © 2018 Qiu Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
