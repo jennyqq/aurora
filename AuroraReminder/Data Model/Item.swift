5b268d6d8d04bfedb0b9f7b7ebb02cea71ca8350
jennyqq <jennifer@drawbridgenetworks.com>
2018-06-19 10:22:10 -0400

    Add model

---
+++ AuroraReminder/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  AuroraReminder
+//
+//  Created by Qiu Zhang on 6/19/18.
+//  Copyright © 2018 Qiu Zhang. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var value: String = ""
+    var done: Bool = false
+}
